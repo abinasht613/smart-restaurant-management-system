caf33a827ae1a5045ca2a8e3d9ff8d14b2bbdad6
abinash <abinash@apexaspire.in>
2025-03-12 19:36:51 +0530

    frontend vue added

---
+++ frontend/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'  // This maps @ to src folder
+  },
+  testMatch: ['**/tests/unit/**/*.spec.js']
+};
